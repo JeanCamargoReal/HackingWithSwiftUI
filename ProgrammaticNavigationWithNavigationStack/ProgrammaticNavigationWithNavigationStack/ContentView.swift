5ef0a0f9940fb66a9db4be82fb2f738e755c8c50
Jean Camargo <jeancamargoreal@icloud.com>
2024-07-12 16:13:06 -0300

    add projeto

---
+++ ProgrammaticNavigationWithNavigationStack/ProgrammaticNavigationWithNavigationStack/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProgrammaticNavigationWithNavigationStack
+//
+//  Created by Jean Paulo Marcel Henrique de Camargo on 12/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProgrammaticNavigationWithNavigationStack/ProgrammaticNavigationWithNavigationStack/ProgrammaticNavigationWithNavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgrammaticNavigationWithNavigationStackApp.swift
+//  ProgrammaticNavigationWithNavigationStack
+//
+//  Created by Jean Paulo Marcel Henrique de Camargo on 12/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ProgrammaticNavigationWithNavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
