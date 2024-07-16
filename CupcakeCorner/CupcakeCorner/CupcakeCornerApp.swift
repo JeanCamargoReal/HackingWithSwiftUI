82e587c5bc0a0d1f56260777e286716b0bd35b60
Jean Camargo <jeancamargoreal@icloud.com>
2024-07-16 09:37:25 -0300

    add projeto CupcakeCorner

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Jean Paulo Marcel Henrique de Camargo on 16/07/24.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
