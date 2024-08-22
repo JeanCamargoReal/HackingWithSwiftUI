680bc75108376451bbc26175af4b271f963e4089
Jean Camargo <jeancamargoreal@icloud.com>
2024-08-22 15:40:49 -0300

    Adding HapticEffects project

---
+++ HapticEffects/HapticEffects/HapticEffectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HapticEffectsApp.swift
+//  HapticEffects
+//
+//  Created by Jean Paulo Marcel Henrique de Camargo on 22/08/24.
+//
+
+import SwiftUI
+
+@main
+struct HapticEffectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
