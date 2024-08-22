25039c20d3bd91a124959c91f23753158a6e47cf
Jean Camargo <jeancamargoreal@icloud.com>
2024-08-22 16:00:19 -0300

    adding haptic effects

--- HapticEffects/HapticEffects/ContentView.swift
+++ HapticEffects/HapticEffects/ContentView.swift
@@ -8,14 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var counter = 0
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Button("Tap Count: \(counter)") {
+            counter += 1
         }
-        .padding()
+//        .sensoryFeedback(.increase, trigger: counter)
+//        .sensoryFeedback(.impact(flexibility: .solid, intensity: 0.9), trigger: counter)
+        .sensoryFeedback(.impact(weight: .heavy, intensity: 1), trigger: counter)
     }
 }
 
