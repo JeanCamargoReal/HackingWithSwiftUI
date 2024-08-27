451e38c7c469aff980ed623322c99146582ebee5
Jean Camargo <jeancamargoreal@icloud.com>
2024-08-27 15:18:14 -0300

    Taking basic order details

---
+++ CupcakeCorner/CupcakeCorner/AddressView.swift
@@ -0,0 +1,20 @@
+//
+//  AddressView.swift
+//  CupcakeCorner
+//
+//  Created by Jean Paulo Marcel Henrique de Camargo on 27/08/24.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    var order: Order
+
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddressView(order: Order())
+}
