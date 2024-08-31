7f498acaa98afde59d194e1bfe458825565c4f73
Jean Camargo <jeancamargoreal@icloud.com>
2024-08-31 16:13:30 -0300

    Creating a custom component with @Binding

---
+++ Bookworm/Bookworm/Exemples/PushButtonExemple.swift
@@ -0,0 +1,43 @@
+//
+//  PushButton.swift
+//  Bookworm
+//
+//  Created by Jean Paulo Marcel Henrique de Camargo on 31/08/24.
+//
+
+import SwiftUI
+
+
+struct PushButtonExemple: View {
+    @State private var rememberMe = false
+
+    var body: some View {
+        VStack {
+            PushButton(title: "Remember Me", isOn: $rememberMe)
+            Text(rememberMe ? "On" : "Off")
+        }
+    }
+}
+
+struct PushButton: View {
+    let title: String
+    @Binding var isOn: Bool
+
+    var onColors = [Color.red, Color.yellow]
+    var offColors = [Color(white: 0.6), Color(white: 0.4)]
+
+    var body: some View {
+        Button(title) {
+            isOn.toggle()
+        }
+        .padding()
+        .background(LinearGradient(colors: isOn ? onColors : offColors, startPoint: .top, endPoint: .bottom))
+        .foregroundStyle(.white)
+        .clipShape(.capsule)
+        .shadow(radius: isOn ? 0 : 5)
+    }
+}
+
+#Preview {
+    PushButtonExemple()
+}
