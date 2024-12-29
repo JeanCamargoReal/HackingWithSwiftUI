5e3bab1314b1089e56443d529af59b2d70387129
Jean Camargo <jeancamargoreal@icloud.com>
2024-12-29 11:12:25 -0300

    Instafilter project

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Jean Paulo Marcel Henrique de Camargo on 29/12/24.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
